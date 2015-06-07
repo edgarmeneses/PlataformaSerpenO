ada8460d5c283e72973df8be5cde4f4188789192
Daniela <Daniela@Daniela-PC>
2015-06-07 10:13:16 -0500

    Modificacion de la tabla de banco

--- src/com/serpen/interfaces/PanelTableBank.java
+++ src/com/serpen/interfaces/PanelTableBank.java
@@ -1,10 +1,12 @@
 package com.serpen.interfaces;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 
 public class PanelTableBank extends Panel {
 
@@ -13,7 +15,12 @@ public class PanelTableBank extends Panel {
 	private Label lblTitle;
 	private Panel pnlTitle;
 	
+	private Panel pnlSearch;
+	private Label lblSearch;
+	private TextField txtSearch;
+	private Button btnSearch;
 	
+
 	public PanelTableBank(){
 		
 		FormLayout formLayout = new FormLayout();
@@ -25,14 +32,24 @@ public class PanelTableBank extends Panel {
 		FormLayout formTitle = new FormLayout();
 		formTitle.setVisible(true);
 		
+		FormLayout formSearch = new FormLayout();
+		formSearch.setVisible(true);
+		
 		HorizontalLayout horizontlTitle = new  HorizontalLayout();
 		horizontlTitle.setVisible(true);
+		
+		HorizontalLayout horizontalSearch = new HorizontalLayout();
+		horizontalSearch.setVisible(true);
 		
 		pnlTitle = new Panel();
 		pnlTitle.setSizeFull();
 		pnlTitle.setWidth("1000");
 		pnlTitle.setHeight("80");
 		
+		pnlSearch = new Panel();
+		pnlSearch.setSizeFull();
+		pnlSearch.setWidth("1000");
+		pnlSearch.setHeight("100");
 		
 		pnlTable = new Panel();
 		pnlTable.setWidth("1000px");
@@ -44,6 +61,24 @@ public class PanelTableBank extends Panel {
 		lblTitle.setWidth("200px");
 		lblTitle.setHeight("50px");
 		lblTitle.setVisible(true);
+		
+		lblSearch = new Label("Tipo de cuenta");
+		lblSearch.setWidth("100px");
+		lblSearch.setHeight("50px");
+		lblSearch.setVisible(true);
+		
+		txtSearch = new TextField();
+		txtSearch.setWidth("150px");
+		txtSearch.setHeight("50px");
+		txtSearch.setRequired(true);
+		txtSearch.setValue("");
+		txtSearch.setNullRepresentation("");
+		txtSearch.setVisible(true);
+		
+		btnSearch = new Button("Buscar");
+		btnSearch.setWidth("150px");
+		btnSearch.setHeight("50px");
+		btnSearch.setVisible(true);
 		
 	    table = new Table();
 	    
@@ -64,12 +99,20 @@ public class PanelTableBank extends Panel {
 		
 		horizontlTitle.addComponent(lblTitle);
 		
+		horizontalSearch.addComponent(lblSearch);
+		horizontalSearch.addComponent(txtSearch);
+		horizontalSearch.addComponent(btnSearch);
+		
+		formSearch.addComponent(horizontalSearch);
+		
 		formTitle.addComponent(horizontlTitle);
 		
 		this.pnlTable.setContent(formTable);
 		this.pnlTitle.setContent(formTitle);
+		this.pnlSearch.setContent(formSearch);
 		
 		formLayout.addComponent(pnlTitle);
+		formLayout.addComponent(pnlSearch);
 		formLayout.addComponent(pnlTable);
 		
 		
